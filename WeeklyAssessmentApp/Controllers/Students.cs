64b37c77c1fbc25c50127370750069eb9e436521
Prashant097 <prashanttrivedi097@gmail.com>
2020-12-04 16:19:10 +0530

    Added Students display view

---
+++ WeeklyAssessmentApp/Controllers/Students.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WeeklyAssessmentApp.Models;
+
+namespace WeeklyAssessmentApp.Controllers
+{
+    public class Students : Controller
+    {
+        public IActionResult Display()
+        {
+            List<Student>  stulist=new List<Student> 
+            {
+            new Student(){Id=101,Name="Pranjal",DOB=DateTime.Parse("06/06/1998")},
+            new Student(){Id=102,Name="Amar",DOB=DateTime.Parse("05/07/1997")},
+            new Student(){Id=103,Name="Anita",DOB=DateTime.Parse("04/08/1996")},
+            new Student(){Id=104,Name="Prateek",DOB=DateTime.Parse("09/09/1995")},
+            new Student(){Id=105,Name="Sharad",DOB=DateTime.Parse("06/08/1997")}
+            };
+            ViewBag.nos = stulist.Count();
+
+            return View(stulist);
+        }
+    }
+}
