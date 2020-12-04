df40584627a24a495c2a61e5bb613f63a910fe81
Prashant097 <prashanttrivedi097@gmail.com>
2020-12-04 16:11:26 +0530

    Added Student Model with Properties

---
+++ WeeklyAssessmentApp/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WeeklyAssessmentApp.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime DOB { get; set; }
+        public double Contact { get; set; }
+
+    }
+}
