fb1d9ac1bbe953cccdb3bb8a64dfbddc8dec1b02
Prashant097 <prashanttrivedi097@gmail.com>
2020-12-04 16:43:48 +0530

    Final Commit

--- WeeklyAssessmentApp/Controllers/Students.cs
+++ WeeklyAssessmentApp/Controllers/Students.cs
@@ -13,11 +13,11 @@ namespace WeeklyAssessmentApp.Controllers
         {
             List<Student>  stulist=new List<Student> 
             {
-            new Student(){Id=101,Name="Pranjal",DOB=DateTime.Parse("06/06/1998")},
-            new Student(){Id=102,Name="Amar",DOB=DateTime.Parse("05/07/1997")},
-            new Student(){Id=103,Name="Anita",DOB=DateTime.Parse("04/08/1996")},
-            new Student(){Id=104,Name="Prateek",DOB=DateTime.Parse("09/09/1995")},
-            new Student(){Id=105,Name="Sharad",DOB=DateTime.Parse("06/08/1997")}
+            new Student(){Id=101,Name="Pranjal",DOB=DateTime.Parse("06/06/1998"),Contact=9874563214},
+            new Student(){Id=102,Name="Amar",DOB=DateTime.Parse("05/07/1997"),Contact=9746312584},
+            new Student(){Id=103,Name="Anita",DOB=DateTime.Parse("04/08/1996"),Contact=8521479635},
+            new Student(){Id=104,Name="Prateek",DOB=DateTime.Parse("09/09/1995"),Contact=7531598462},
+            new Student(){Id=105,Name="Sharad",DOB=DateTime.Parse("06/08/1997"),Contact=9632587412}
             };
             ViewBag.nos = stulist.Count();
 
--- WeeklyAssessmentApp/Startup.cs
+++ WeeklyAssessmentApp/Startup.cs
@@ -46,7 +46,7 @@ namespace WeeklyAssessmentApp
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Home}/{action=Home}/{id?}");
             });
         }
     }
